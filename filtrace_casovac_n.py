c86ac7c219ed7209d108270c3bc2a578cb163892
JiriKursky <44495206+JiriKursky@users.noreply.github.com>
2019-05-21 12:33:01 +0200

    Update filtrace_casovac_n.py

--- filtrace_casovac_n.py
+++ filtrace_casovac_n.py
@@ -4,18 +4,33 @@
 # me trapeni: 
 # from datetime import datetime - nelze pouzit
 
+# pocet cyklu za 24 hodin = 'n'
+POCET_CYKLU = 8
+
+# nastaveny pocatecni cas
+# entity musi byt definovane takto input_datetime.filtrace_t_1, input_datetime.filtrace_t_2 ... az do POCET_CYKLU
+ENTITY_FILTRACE_T = 'filtrace_t'
+
+
+# doba behu, musi byt mensi nez 59 - nehlidano(!)
+# entity musi byt input_number.filtrace_casovac_vypnuti_1, input_number.filtrace_casovac_vypnuti_2 ... az do POCET_CYKLU
+ENTITY_DOBA_BEHU = 'filtrace_casovac_vypnuti'
+
+# co se spusti nebo vypne sluzbou input_boolean.turn_on nebo input_boolean.turn_off
+ENTITY_AKCE = 'filtrace_zapni'
+
 # aktualni datum, cas
 praveTed = datetime.datetime.now()    
 
 hledam = True
 i = 1
 
-while hledam and i<=8 :
+while hledam and i <= POCET_CYKLU :
 
     sI = str(i)
 
     # vrati string
-    sCasovac = hass.states.get("input_datetime.filtrace_t_" + sI).state
+    sCasovac = hass.states.get("input_datetime." + ENTITY_FILTRACE_T + "_" + sI).state
 
     # hodila by se funkce strptime, ale bez sance
     # casovac = datetime.strptime(sCasovac, '%H:%M:%S') 
@@ -32,8 +47,8 @@ while hledam and i<=8 :
     # vlastni prevod
     casovac = praveTed.replace(hour=hodina, minute=minuta, second=vterina)
 
-    # ten float je tam z duvou, desetinne tecky, jinak to blbne
-    dobaMinut = int(float(hass.states.get("input_number.filtrace_casovac_vypnuti_" + sI).state))
+    # ten float je tam z duvodu, desetinne tecky, jinak to blbne
+    dobaMinut = int(float(hass.states.get("input_number." + ENTITY_DOBA_BEHU + "_" + sI).state))
 
     doCasu = casovac + datetime.timedelta(minutes = dobaMinut)
 
@@ -42,11 +57,15 @@ while hledam and i<=8 :
         # aktivni cas a nasel jsem
         hledam = False
     i = i + 1
-# konec hledani casoveho intervalu        
+# end while
+# konec hledani casoveho intervalu, pokud je aktivni ma 'hledam' hodnotu false        
 
 if hledam :
     toDo = "turn_off"    
 else:
     toDo = "turn_on"
+
+# pro ucely ladeni
 # logger.info(toDo)
-hass.services.call("input_boolean", toDo, {"entity_id": "input_boolean.filtrace_zapni"}, False)                   
+
+hass.services.call("input_boolean", toDo, { "entity_id": "input_boolean." + ENTITY_AKCE }, False)                   
